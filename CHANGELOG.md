893aa7c2df4b3517ca18378f0ca2b5b2eccd8fd5
Nutthapat Pongtanyavichai <contact@leomotors.me>
2026-01-06 12:31:10 +0700

    unbundle build for rsc

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## [0.2.0] - 2025-01-06
+
+- unbundle for better RSC support
+- drop CJS build
+
 ## [0.1.0] - 2025-12-31
 
 - test publish
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["./src/index.ts"],
-  format: ["esm", "cjs"],
+  format: ["esm"],
+  unbundle: true,
   dts: true,
   platform: "neutral",
   clean: true,
